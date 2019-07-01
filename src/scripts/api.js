675c19dc790f41ca8d8e4aa14ed001f2b196efe8
Tyler Carpenter <tcarp55@gmail.com>
2019-07-01 13:50:29 -0500

    added some of the API functions to add delete and get data.

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,12 +1,34 @@
 const API = {
-    getData(entity, id){
-        if(id){
-            return fetch(`http://localhost:8088/${entity}/${id}`)
-            .then(response => response.json())
-        }
-        return fetch(`http://localhost:8088/${entity}`)
-        .then(response => response.json())
+  getData(entity, id) {
+    if (id) {
+      return fetch(`http://localhost:8088/${entity}/${id}`)
+      .then(response =>response.json())
     }
-}
+    return fetch(`http://localhost:8088/${entity}`)
+    .then(response => response.json())
+  },
+  addData(entity, object) {
+    return fetch(`http://localhost:8088/${entity}`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(object)
+    });
+  },
+    deleteData(entity, id) {
+    return fetch(`http://localhost:8088/${entity}/${id}`, {
+      method: "Delete",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+    });
+  }
+};
 
-export {API}
+//?_expand=user use when trying to epand any entity and get the user info.
+//example API.getData("events", "?_expand=user")
+
+export { API };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -12,4 +12,29 @@ API.getData("users", 1)
     console.log(thisThing)
 })
 
+API.getData("messages", "?_expand=user")
+.then(data => {
+    console.log("help")
+    let thisThing = data
+    console.log(thisThing)
+})
+//  API.addData("messages",createMessage(2, "i typed something"))
+//  .then(data => {
+//      API.getData("messages").then(things => {
+//          console.log(things)
+//      })
+//  })
+//  console.log(createMessage(1, "i typed something"))
+API.deleteData("messages", 16).then(data =>
+    API.getData(messages).then( newData => console.log(newData))
+    )
+
+
+
+function createMessage(userId, message){
+    return {
+        userId: userId,
+        message: message
+    }
+}
 console.log("something")
