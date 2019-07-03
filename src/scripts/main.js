05e0408829fcfa680d181c1615f9eea08b6f64a3
Danny Barker <barkerdanny17@yahoo.com>
2019-07-03 16:05:34 -0500

    got friend relationship working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,24 @@
 import {createNav, createDashboard} from "./mainComponent.js"
 import {mainEntryToDom, dashToDOM} from "./mainEntryToDom.js"
+import {API} from "./api.js"
 if (sessionStorage.getItem("userId")) {
     mainEntryToDom(createNav(), createDashboard())
 } else {
     dashToDOM()
 }
+
+API.getData("friends").then(data =>{
+    let friendArr = data.filter( key => {
+        for (let foo of Object.entries(key)) {
+            let userArr = foo[0]
+            let splitArr = userArr.split("-")
+            let id = +sessionStorage.getItem("userId")
+            if (splitArr[0] === "userId" && foo[1] !== id) {
+            //     let id = key.newString
+            //     console.log(id);
+             API.getData("users", foo[1]).then(data => console.log(data))
+            }
+        }
+    })
+
+})
