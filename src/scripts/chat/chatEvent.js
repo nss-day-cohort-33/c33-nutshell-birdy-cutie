36fb6034dd5073720266defac9486878c15582d5
krystal gates <krystal_sabrina@yahoo.com>
2019-07-03 15:04:43 -0500

    validation for message text and recent mesage at bottom

--- src/scripts/chat/chatEvent.js
+++ src/scripts/chat/chatEvent.js
@@ -1,12 +1,14 @@
 import { API } from "../api";
 import { messageComponent } from "./messageComponent.js"
-import { buildMessageObj } from "./chatHelpers.js"
+import { buildMessageObj, messageInputLength } from "./chatHelpers.js"
 
 function messageBtnListener(){
     let userId = +sessionStorage.getItem("userId")
     let username = ""
     let message = document.querySelector("#message-input").value
     let timeStamp = Date.now()
+    let messageLength = messageInputLength(message)
+    if (messageLength === true ){
     API.getData("users", userId)
     .then(data => {
         username = data.username
@@ -15,15 +17,16 @@ function messageBtnListener(){
             API.getData("messages")
             .then(data => {
                 let timeStampSort = data.sort((time1, time2)=>{
-                    return time2 - time1
+                    return time1 - time2
                 })
-                timeStampSort.forEach(obj => {
+                for(let i = timeStampSort.length - 10; i < timeStampSort.length; i++) {
                     let chatPointer = document.querySelector("#chat-div")
-                    chatPointer.appendChild(messageComponent(obj))
-                });
+                    chatPointer.appendChild(messageComponent(timeStampSort[i]))
+                };
             })
         })
     })
+}
 }
 
 export {messageBtnListener}
