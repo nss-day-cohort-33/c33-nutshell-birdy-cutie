1c661ccfdbbb39802061985a88d34d322e4028ae
Tyler Carpenter <tcarp55@gmail.com>
2019-07-08 11:04:14 -0500

    added populateChat func to popDom func

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import {createNav, createDashboard} from "./mainComponent.js"
 import {mainEntryToDom, dashToDOM} from "./mainEntryToDom.js"
 import {addArticleToDom } from "./article/addArticleToDOM.js"
 import {populateTaskContainer } from "./tasks/taskToDom.js"
+import { entriesToChat } from "./chat/entriesToChat.js"
 if (sessionStorage.getItem("userId")) {
     mainEntryToDom(createNav(), createDashboard())
     populateDom()
@@ -14,6 +15,7 @@ if (sessionStorage.getItem("userId")) {
 function populateDom(){
     addArticleToDom()
     populateTaskContainer()
+    entriesToChat()
 }
 
 export {populateDom }
