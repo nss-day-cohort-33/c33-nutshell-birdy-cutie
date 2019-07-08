dc94216212c8c7740f892fb1cc243f8ba6e630c1
Danny Barker <barkerdanny17@yahoo.com>
2019-07-08 14:02:34 -0500

    fixed where event and friend divs were attaching too.

--- src/scripts/friends/addFriendsToDOM.js
+++ src/scripts/friends/addFriendsToDOM.js
@@ -1,15 +1,13 @@
-import {createNav} from "../mainComponent.js"
 
 const createFriendDiv = () => {
-  let dashContainer = document.querySelector("#dashboard-container")
+  let dashContainer = document.querySelector("#data-container")
   let friendDiv = document.createElement("div")
   friendDiv.setAttribute("id", "friend-div")
-  friendDiv.innerHTML = "<h4>Friends</h4>"
+  friendDiv.innerHTML = "<h3>Friends</h3>"
   let listOfFriends = document.createElement("article")
   listOfFriends.innerHTML = "Add some friends!"
   listOfFriends.setAttribute("id", "friendsList")
   friendDiv.appendChild(listOfFriends)
-  dashContainer.appendChild(createNav())
   dashContainer.appendChild(friendDiv)
 }
 export {createFriendDiv}
